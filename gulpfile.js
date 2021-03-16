fc3a01b0b636128d74378a1ce3c3031e60ebf50f
Ben Girardet <ben@platform5.ch>
2021-03-16 11:47:28 +0100

    remove sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require("gulp");
-var sass = require("gulp-sass");
 
 gulp.task("copy:html", gulp.series(function() {
   return gulp.src("src/**/*.html")
@@ -28,14 +27,4 @@ gulp.task("copy:json", gulp.series(function() {
     .pipe(gulp.dest("dist/system"));
   }));
 
-gulp.task("sass", gulp.series(function() {
-  return gulp.src("src/**/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(gulp.dest("dist/amd"))
-    .pipe(gulp.dest("dist/commonjs"))
-    .pipe(gulp.dest("dist/es2015"))
-    .pipe(gulp.dest("dist/native-modules"))
-    .pipe(gulp.dest("dist/system"));
-  }));
-
-gulp.task("default", gulp.series(["copy:html", "copy:css", "copy:json", "sass"]));
+gulp.task("default", gulp.series(["copy:html", "copy:css", "copy:json"]));

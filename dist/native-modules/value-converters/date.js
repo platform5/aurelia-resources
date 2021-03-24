6923cffb6e6629a27a5f0c4ffa7094e6a0c1bf29
Ben Girardet <ben@platform5.ch>
2021-03-24 09:37:44 +0100

    Improve date value converter

--- dist/native-modules/value-converters/date.js
+++ dist/native-modules/value-converters/date.js
@@ -6,7 +6,33 @@ var DateValueConverter = /** @class */ (function () {
         if (format === void 0) { format = 'DD.MM.YYYY'; }
         if (!date)
             return '';
-        var m = moment(date);
+        var m;
+        if (typeof date === 'string') {
+            if (date.length === 10 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-') {
+                m = moment(date, 'DD-MM-YYYY');
+            }
+            else if (date.length === 8 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-') {
+                m = moment(date, 'DD-MM-YY');
+            }
+            else if (date.length === 10 && date.substr(2, 1) === '/' && date.substr(5, 1) === '/') {
+                m = moment(date, 'DD/MM/YYYY');
+            }
+            else if (date.length === 8 && date.substr(2, 1) === '/' && date.substr(5, 1) === '/') {
+                m = moment(date, 'DD/MM/YY');
+            }
+            else if (date.length === 10 && date.substr(2, 1) === '.' && date.substr(5, 1) === '.') {
+                m = moment(date, 'DD.MM.YYYY');
+            }
+            else if (date.length === 8 && date.substr(2, 1) === '.' && date.substr(5, 1) === '.') {
+                m = moment(date, 'DD.MM.YY');
+            }
+            else if (date.length > 10 && date.indexOf('T') !== -1 && date.indexOf('+') !== -1) {
+                m = moment(date);
+            }
+            else {
+                m = moment(date);
+            }
+        }
         if (format === 'nice') {
             var diff = m.diff(moment(), 'days');
             if (diff < 14) {

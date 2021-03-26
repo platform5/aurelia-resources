a7fb254645abaf0883a40d1caba8261f56053398
Tamu <5020311+Tamu@users.noreply.github.com>
2021-03-26 10:04:14 +0100

    Add date helper

--- src/value-converters/date.ts
+++ src/value-converters/date.ts
@@ -1,36 +1,12 @@
 import * as moment from 'moment';
+import { DateHelper } from '../helpers/date';
 
 export class DateValueConverter {
   toView(date: string | moment.Moment, format: string = 'DD.MM.YYYY') {
-    if (!date) return '';
-    let m: moment.Moment;
-    if (typeof date === 'string') {
-      if (date.length === 10 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-') {
-        m = moment(date, 'DD-MM-YYYY');
-      } else if (date.length === 8 && date.substr(2, 1) === '-' && date.substr(5, 1) === '-') {
-        m = moment(date, 'DD-MM-YY');
-      } else if (date.length === 10 && date.substr(2, 1) === '/' && date.substr(5, 1) === '/') {
-        m = moment(date, 'DD/MM/YYYY');
-      } else if (date.length === 8 && date.substr(2, 1) === '/' && date.substr(5, 1) === '/') {
-        m = moment(date, 'DD/MM/YY');
-      } else if (date.length === 10 && date.substr(2, 1) === '.' && date.substr(5, 1) === '.') {
-        m = moment(date, 'DD.MM.YYYY');
-      } else if (date.length === 8 && date.substr(2, 1) === '.' && date.substr(5, 1) === '.') {
-        m = moment(date, 'DD.MM.YY');
-      } else if (date.length > 10 && date.indexOf('T') !== -1 && date.indexOf('+') !== -1) {
-        m = moment(date);
-      } else {
-        m = moment(date);
-      }
-    } else if (!moment.isMoment(date)) {
-      m = moment(date);
-      if (!m.isValid) {
-        return '';
-      }
-    } else {
-      m = date;
+    const m = DateHelper.moment(date);
+    if (!m) {
+      return '';
     }
-
 
     if (format === 'nice') {
       let diff = m.diff(moment(), 'days');
